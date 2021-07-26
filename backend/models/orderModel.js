95e0b8effd58b1d96f783702bb0499837b6a081e
asumithd <sumithdeepan@gmail.com>
2021-07-26 21:30:21 +0530

    redux

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,16 +1,5 @@
 import mongoose from "mongoose";
 
-// const reviewSchema = mongoose.Schema(
-//   {
-//     name: { type: String, required: true },
-//     rating: { type: Number, required: true },
-//     comment: { type: String, required: true },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
-
 const orderSchema = mongoose.Schema(
   {
     user: {
@@ -19,45 +8,47 @@ const orderSchema = mongoose.Schema(
       ref: "User",
     },
     orderItems: [
-        name:{
-      type: String,
-      required: true,
-    },
-        qty:{
-      type: Number,
-      required: true,
-    },
-        image:{
-      type: String,
-      required: true,
-    },
-        price:{
-      type: Number,
-      required: true,
-    },
-        product:{
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "Product",
-    }
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        qty: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
     ],
     shippingAddress: {
-      address:{
-      type: String,
-      required: true,
-    },
-        city:{
-      type: String,
-      required: true,
-    },
-        postalCode:{
-      type: String,
-      required: true,
-    },
-        country:{
-      type: String,
-      required: true,
-    }
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
     },
     paymentMethod: {
       type: String,
@@ -65,14 +56,17 @@ const orderSchema = mongoose.Schema(
     },
     paymentResult: {
       id: {
-      type: String
-    },status: {
-      type: String
-    },update_time: {
-      type: String
-    },email_address: {
-      type: String
-    }
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+      update_time: {
+        type: String,
+      },
+      email_address: {
+        type: String,
+      },
     },
     taxPrice: {
       type: Number,
@@ -92,18 +86,18 @@ const orderSchema = mongoose.Schema(
     isPaid: {
       type: Boolean,
       required: true,
-      default: false
+      default: false,
     },
     paidAt: {
-      type: Date
+      type: Date,
     },
     deliveredAt: {
-      type: Date
+      type: Date,
     },
     isDelivered: {
       type: Boolean,
       required: true,
-      default: false
+      default: false,
     },
   },
   {
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,31 @@
-import express from 'express'
-import products from './data/products.js'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoute from "./routes/productRoutes.js";
+import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
+dotenv.config();
 
-dotenv.config()
-
-connectDB()
+connectDB();
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('API is running');
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.get('/api/products',(req,res)=>{
-    res.json(products);
-})
+app.use("/api/products", productRoute);
 
-app.get('/api/product/:id',(req,res)=>{
-    const product = products.find(p=>p._id===req.params.id);
-    res.json(product);
-})
-const PORT=process.env.PORT || 5000
-const Environment=process.env.NODE_ENV || 5000
-app.listen(PORT,console.log(`Server Running in ${Environment} mode on Port ${PORT}`.yellow.bold))
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+const Environment = process.env.NODE_ENV || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server Running in ${Environment} mode on Port ${PORT}`.yellow.bold
+  )
+);
